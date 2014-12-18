a0f6ce0fd23a2f1be818f2d36bc1ef5bfc1cfaa9
Pizzorni <Giorgio@pizzorni.com>
2014-12-18 15:27:43 -0500

    Added some benchmarking tests

--- sort.py
+++ sort.py
@@ -1,9 +1,30 @@
 import random
+import time
+import copy
 
 def main():
-	x = random.sample(range(1000),100)
-	print(x)
-	print(Bogobogosort(x))
+	x = random.sample(range(1000),10)
+	y = copy.copy(x)
+	z = copy.copy(x)
+
+	t0 = time.time()
+	x.sort()
+	t1 = time.time()
+	normsort = t1-t0
+
+	t0 = time.time()
+	Bogosort(y)
+	t1 = time.time()
+	bogotime = t1-t0
+
+	t0 = time.time()
+	Bogobogosort(z)
+	t1 = time.time()
+	bogobogotime = t1-t0
+
+
+	print("Normal sort: " + str(normsort) + "\n Bogosort: "
+	 + str(bogotime) + "\n Bogobogosort: " + str(bogobogotime))
 
 
 def Bogosort(list):
